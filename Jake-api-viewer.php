24cef2d741988774f78960077df3f28c72413eb3
jake <jakecolburn.sythe@gmail.com>
2019-10-15 22:54:34 -0400

    initial commit

---
+++ Jake-api-viewer.php
@@ -0,0 +1,14 @@
+<?php
+/*
+ * @package JakeApiViewer
+ */
+/*
+Plugin Name: Jake Api Viewer
+Plugin URI: http://github.com/sythe7448
+Description: A test plugin to learn about calling and displaying APIs
+Version: 1.0
+Author: Jake Colburn
+Author URI: http://github.com/sythe7448
+License: GPL2
+Text Domain: jake-api-viewer
+*/
