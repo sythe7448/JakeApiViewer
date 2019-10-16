533f400e18578ce208aac3707e0bfa05720601be
jake <jakecolburn.sythe@gmail.com>
2019-10-16 15:00:01 -0400

    initial commit

--- Jake-api-viewer.php
+++ Jake-api-viewer.php
@@ -12,3 +12,23 @@ Author URI: http://github.com/sythe7448
 License: GPL2
 Text Domain: jake-api-viewer
 */
+
+if ( ! defined( 'ABSPATH')) {
+    die;
+}
+
+require 'vendor/autoload.php';
+
+function getEveAlliances(){
+    $client = new GuzzleHttp\Client();
+    $res = $client->request('GET', 'https://esi.evetech.net/latest/alliances/?datasource=tranquility');
+    $allianceArray = json_decode($res->getBody());
+    ob_start();
+    for ($i = 0; $i < 10; $i++) {
+        $result = $client->request('GET', "https://esi.evetech.net/latest/alliances/{$allianceArray[$i]}/?datasource=tranquility");
+        $allianceData = json_decode($result->getBody());
+        echo "<p>{$allianceData->name}</p>";
+    }
+    return ob_get_clean();
+}
+add_shortcode( 'eveAlliances', 'getEveAlliances' );
